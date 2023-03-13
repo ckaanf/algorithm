ffca3ae4c3b8a280509050bd24a6a2de096bfe16
Jin Soo Kim <57103133+frontLine-kim@users.noreply.github.com>
2023-03-13 16:49:35 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        int count = 0;
+        for(String s1Word : s1){
+            for(String s2Word : s2){
+                if(s2Word.equals(s1Word)) count ++;
+            }
+        }
+        return count;
+    }
+}
