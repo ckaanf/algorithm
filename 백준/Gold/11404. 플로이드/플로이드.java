ca1f7bbb92f10605e0663a5a4deea81d0ce3a249
Jin Soo Kim <57103133+frontLine-kim@users.noreply.github.com>
2023-07-26 19:48:35 +0900

    [Gold IV] Title: 플로이드, Time: 472 ms, Memory: 44460 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int max = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n + 1][n + 1];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+
+                arr[i][j] =max;
+
+                if(i == j){
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int price = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = Math.min(arr[a][b], price);
+        }
+
+        // 플로이드-와샬 알고리즘 검색..ㅠ
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    // 최단경로 초기화
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(arr[i][j] == max){
+                    arr[i][j] = 0;
+                }
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
