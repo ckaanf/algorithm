a8397ea4576a3aa009725a237c160d46671699a3
Jin Soo Kim <57103133+ckaanf@users.noreply.github.com>
2023-08-24 20:09:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(int n) {
+        int dp[] = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i =3; i<2001; i++){  
+            dp[i]= (dp[i-2]+dp[i-1])%1234567;
+        }          
+        return dp[n];
+    }
+}
+
+// n = 1 return = 1   [1]
+// n = 2 return = 2   [1],[2]
+// n = 3 return = 3   [1,1,1],[1,2],[2,1]
+// n = 4 return = 5   [1,1,1,1],[1,2,1],[1,1,2],[2,1,1],[2,2]
+// n = 5 return = 8
+// n = 6 return = 13
+// n = 7 return = 18           
+// dp[3] = dp[2]+1 -> dp[1];
+// dp[4] = dp[3]+2;
+// dp[5] = dp[4]+3;
+// dp[6] = dp[5]+5;
+// dp[7] = dp[6]+5?
