6d95034173b48557d2b10bbc03b9fcdf5352ef6a
Jin Soo Kim <57103133+ckaanf@users.noreply.github.com>
2024-06-14 10:51:55 +0900

    Time: 35 ms (82.95%), Space: 60.1 MB (44.92%) - LeetHub

--- LeetCode/0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ LeetCode/0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
 		Arrays.sort(nums);
-		int cnt = 0;
-		for(int i = 0; i < nums.length-1; i++) {
-			while(nums[i] > nums[i+1]) {
-				nums[i+1] = nums[i+1] + 1 ;
-				cnt++;
+		int cnt = nums[0];
+		int result = 0;
+		for(int i= 1 ;i<nums.length ;i++){
+			cnt++;
+			if(cnt <= nums[i]){
+				cnt = nums[i];
+			}else{
+				result += cnt - nums[i];
 			}
-			if(nums[i] == nums[i+1]) {
-				nums[i + 1] = nums[i + 1] + 1;
-				cnt++;
-			}
-        }
-        return cnt;
+		}
+		
+        return result;
     }
 }
