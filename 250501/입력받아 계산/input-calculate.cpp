f344ac30b03c239b7e2ae91cddfcac1c3157dce2
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-05-02 09:56:05 +0900

    Title: 입력받아 계산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250501/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a;
+    b = a + 2;
+    cout << b << endl;
+    return 0;
+}
