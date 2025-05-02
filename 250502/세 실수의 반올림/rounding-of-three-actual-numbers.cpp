f7f20f2877f67c293cd53d37fcc7b308819cfd04
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-05-02 10:03:56 +0900

    Title: 세 실수의 반올림, Time: 3ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250502/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a, b, c;
+    cin >> a >> b >> c;
+    
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
