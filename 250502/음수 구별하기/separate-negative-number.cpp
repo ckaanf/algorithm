1309a4b62257e48aefbbbc669ce6a1b63d498dd9
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-05-02 11:15:05 +0900

    Title: 음수 구별하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250502/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    if (N < 0) {
+        cout << N << endl;
+        cout << "minus" << endl;
+    } else cout << N << endl;
+    return 0;
+}
