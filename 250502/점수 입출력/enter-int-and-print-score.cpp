bcd50f2e48b1c5f9e88c7f4783fbff7d9c9d0880
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-05-02 09:54:38 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250502/점수 입출력/enter-int-and-print-score.cpp
+++ 250502/점수 입출력/enter-int-and-print-score.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main() {
     // Please write your code here.
     int N;
-    cin >> N ;
-
-    cout << "Your score is " + N + "point." << endl;
+    cin >> N;
+    cout << "Your score is " << N << "point." << endl;
     return 0;
 }
