53046f3bb0f35fc255c565e51d3468d3bb7c7d43
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-05-08 17:24:26 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250508/비교 연산/comparison-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    cout << (A >= B) << endl;
+    cout << (A > B) << endl;
+    cout << (A <= B) << endl;
+    cout << (A < B) << endl;
+    cout << (A == B) << endl;
+    cout << (A != B) << endl;
+    return 0;
+}
