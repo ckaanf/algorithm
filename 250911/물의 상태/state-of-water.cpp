4afeb387349964da25cdb7be80ea87a648458da4
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-09-11 09:39:44 +0900

    Title: 물의 상태, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250911/물의 상태/state-of-water.cpp
+++ 250911/물의 상태/state-of-water.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    cin >> N
+    int N;
+    cin >> N;
+
+    if (N > 100) {
+        cout << "vapor";
+    } else if (N < 0) {
+        cout << "ice"
+    } else {
+        cout << "water"
+    }
     return 0;
 }
