ad26082b324534615991b1cac473b0ac5b53f048
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-09-12 13:36:14 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.06 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> my_strings, vector<vector<int>> parts) {
+    string answer = "";
+    for (int i = 0; i < my_strings.size(); i++) {
+        int start = parts[i][0];
+        int end   = parts[i][1];
+        answer += my_strings[i].substr(start, end - start + 1);
+    }
+    return answer;
+}
