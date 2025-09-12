9fbc5381b394c2b48aaab1042b952d3835417471
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-09-12 13:58:37 +0900

    [level 0] Title: 수 조작하기 2, Time: 445.23 ms, Memory: 7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for(int i = 0; i < numLog.size() - 1; i++) {
+        int diff = numLog[i+1] - numLog[i];
+        if (diff == 1) {
+            answer = answer + "w";
+        } else if (diff == -1) {
+            answer = answer + "s";
+        } else if (diff == 10) {
+            answer = answer + "d";
+        } else {
+            answer = answer + "a";
+        }
+    }
+    return answer;
+}
